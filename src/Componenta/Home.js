e58f6a6bc35bc09e2bbaba48d588bd7c00f44832
npx <smuralikrishna@lyrostech.com>
2024-03-14 17:23:35 +0530

    committed

--- src/Componenta/Home.js
+++ src/Componenta/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Usecontext1 } from '../UserContext/Usercontext1'
 import { useContext } from 'react'
+// import { Usecontext1 } from '../UserContext/Usercontext1'
 
 function Home() {
-  const {UserName1,SetuserName1} = useContext(Usecontext1)
+  const {flag,setFlag,UserName1,SetuserName1} = useContext(Usecontext1)
   console.log(UserName1)
   return (
     <>
     <div>Home</div>
     <div>{UserName1}</div>
+    <button onClick={() => setFlag(() => 0)}>Logout</button>
     
     </>
     
